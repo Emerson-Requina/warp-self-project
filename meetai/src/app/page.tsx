3f6abb4b334b632a61ff516c2e5afa22d7b57b2d
Emerson <ercontreras@up.edu.ph>
2025-06-26 10:35:05 +0800

    01: Setup

---
+++ meetai/src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+
+export default function Home() {
+  return (
+    // <div className="text-4xl font-bold text-green-500">Hello World!</div>
+    <Button variant="destructive" className="bg-red-500 hover:bg-red-600">
+      Click Me
+    </Button>
+  );
+}
