6af8d7d06df48ecf817cbad0419ef6d159b926bd
Emerson <ercontreras@up.edu.ph>
2025-06-28 11:05:51 +0800

    10: Responsive Dialog

--- meetai/src/modules/dashboard/ui/components/dashboard-command.tsx
+++ meetai/src/modules/dashboard/ui/components/dashboard-command.tsx
@@ -1,5 +1,12 @@
-import { CommandDialog, CommandInput, CommandItem, CommandList } from "@/components/ui/command"
 import { Dispatch, SetStateAction } from "react";
+
+import { 
+    CommandResponsiveDialog, 
+    CommandInput, 
+    CommandItem, 
+    CommandList 
+} from "@/components/ui/command"
+
 
 interface Props {
     open: boolean;
@@ -8,7 +15,7 @@ interface Props {
 
 export const DashboardCommand = ({ open, setOpen }: Props) => {
     return (
-        <CommandDialog open={open} onOpenChange={setOpen}>
+        <CommandResponsiveDialog open={open} onOpenChange={setOpen}>
             <CommandInput 
                 placeholder="Find a meeting or agent"
             />
@@ -16,8 +23,11 @@ export const DashboardCommand = ({ open, setOpen }: Props) => {
                 <CommandItem>
                     Test
                 </CommandItem>
+                <CommandItem>
+                    Test2
+                </CommandItem>
             </CommandList>
-        </CommandDialog>
+        </CommandResponsiveDialog>
     );
 };
 
